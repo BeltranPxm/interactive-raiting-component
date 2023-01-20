11333972f0347308213e3188ab0d81369e6e63a4
BeltranPxm <beltranpxm@outlook.com>
2023-01-20 13:53:09 -0600

    Mark HTML completed

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Delcaración de variables
+
+// Declaración de funciones
+const documentLoaded = () => {
+  console.log('Documento Cargado');
+}
+
+
+//Asignación de eventos
+window.addEventListener('DOMContentLoaded', documentLoaded)
